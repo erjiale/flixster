dcff35bb4236ec501a8a150371d9eaf0339aba1b
Jiale Qiu <jiale98chinoguay@gmail.com>
2019-10-18 02:12:12 -0400

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Jiale Qiu on 10/18/19.
+//  Copyright © 2019 jiale98chinoguay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
